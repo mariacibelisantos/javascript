59d862f5a426a696bea92c54f6592e2bee1bc1fa
mariacibelisantos <mariacibeli475@gmail.com>
2024-07-04 11:57:51 -0300

    atividades

---
+++ imc-função/script.js
@@ -0,0 +1,8 @@
+// value = pega o valor dentro da caixinha
+function calcularIMC(){
+    const peso = parseFloat(document.getElementById('peso').value)
+    const altura = parseFloat(document.getElementById('altura').value)
+
+    const imc = peso / altura ** 2
+    document.getElementById("imcResultado").textContent = imc
+}
